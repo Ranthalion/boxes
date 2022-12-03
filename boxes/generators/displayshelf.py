6a188126e98a37e8afddc48d14ef2a8513550383
ranthalion <ranthalion@hotmail.com>
2022-12-03 16:35:11 -0500

    fixed slope and holes

--- boxes/generators/displayshelf.py
+++ boxes/generators/displayshelf.py
@@ -43,9 +43,14 @@ class DisplayShelf(Boxes): # change class name here and below
         self.argparser.add_argument(
             "--slope_top", action="store", type=boolarg, default=True,
             help="Slope the sides a the top by front wall height")
+        self.argparser.add_argument(
+            "--hole_diameter", action="store", type=float, default=0,
+            help="Hole diamater for running cords through the shelves and side walls. A value of 0 will not generate any holes.")
 
-    def side(self):
 
+    def generate_finger_holes(self, a=None, b=None, c=None):
+
+        print(f'a={a}, b={b}, c={c}')
         t = self.thickness
         a = math.radians(self.angle)
 
@@ -58,36 +63,60 @@ class DisplayShelf(Boxes): # change class name here and below
             pos_x += math.cos(-a) * (self.sl+0.5*t) + math.sin(a)*0.5*t
             pos_y += math.sin(-a) * (self.sl+0.5*t) + math.cos(a)*0.5*t
             self.fingerHolesAt(pos_x, pos_y, self.frontWallHeight, 90-self.angle)
+            self.hole_at(self.hole_diameter*1.5, pos_y+self.hole_diameter, self.hole_diameter/2)
 
     def generate_sides(self, width, height):
-
         if self.slope_top:
+            top_segment_height = height/self.num
+            unmodified_segment_height = height - top_segment_height
             a = math.radians(self.angle)
-            lip_height = self.frontWallHeight * math.cos(a)
-            hypotenuse = (height - lip_height) / math.sin(a)
-            top_width = width - math.sqrt((hypotenuse ** 2) - ((height - lip_height) * 2))
+            lip_height = self.frontWallHeight #* math.cos(a)
+            m = top_segment_height - lip_height
+            hypotenuse = m / math.sin(a)
+            top_width = width - math.sqrt((hypotenuse ** 2) - (m ** 2))
+            missing_top = 0
 
-            borders = [width, 90, lip_height, self.angle, hypotenuse, 180+self.angle, top_width, 180+90, height, 0]
+            if (top_width <= 0):
+                modified_hypotenuse = width / math.cos(a)
+                m = math.sqrt((modified_hypotenuse ** 2) - (width ** 2))
+                modified_height = unmodified_segment_height + lip_height + m
+                borders = [width, 90, unmodified_segment_height + lip_height, 90-self.angle, modified_hypotenuse, 90+self.angle, modified_height, 90]
+            else:
+                missing_top = math.sqrt((hypotenuse ** 2) - (m ** 2))
+                borders = [width, 90, unmodified_segment_height + lip_height, 90-self.angle, hypotenuse, self.angle, top_width, 90, height, 90]
 
-            self.polygonWall(borders, edge='e', callback=[self.side], move="up")
-            self.polygonWall(borders, edge='e', callback=[self.side], move="up")
+            if self.debug:
+                labelText = f'width: {width}, top_width: {top_width}, missing_top: {missing_top}, expected: {width-top_width}\nhypotenuse: {hypotenuse}, lip_height: {lip_height}, m: {m}, unmodified_segment_height: {unmodified_segment_height}'
+                self.rectangularWall(width,height, move="up", label=labelText)
+            self.polygonWall(borders, edge='f', callback=[self.generate_finger_holes], move="up")
+            self.polygonWall(borders, edge='f', callback=[self.generate_finger_holes], move="up")
         else:
             edges = "eeee"
             if self.include_back:
                 edges = "eeeF"
-            self.rectangularWall(width, height, edges, callback=[self.side], move="up", label="left side")
-            self.rectangularWall(width, height, edges, callback=[self.side], move="up", label="right side")
+            self.rectangularWall(width, height, edges, callback=[self.generate_finger_holes], move="up", label="left side")
+            self.rectangularWall(width, height, edges, callback=[self.generate_finger_holes], move="up", label="right side")
 
     def generate_shelves(self):
+        cb = [lambda : self.hole_at(self.x/2, self.sl-(self.hole_diameter), self.hole_diameter/2)]
         if self.frontWallHeight:
             for i in range(self.num):
-                self.rectangularWall(self.x, self.sl, "ffef", move="up", label="shelf")
-                self.rectangularWall(self.x, self.frontWallHeight, "Ffef", move="up", label="front lip")
+                self.rectangularWall(self.x, self.sl, "ffef", callback=cb, move="up", label=f"shelf {i+1}")
+                self.rectangularWall(self.x, self.frontWallHeight, "Ffef", move="up", label=f"front lip {i+1}")
         else:
             for i in range(self.num):
-                self.rectangularWall(self.x, self.sl, "Efef", move="up", label="shelf")
+                self.rectangularWall(self.x, self.sl, "Efef", callback=cb, move="up", label=f"shelf {i+1}")
+
+    def hole_at(self, x, y, r):
+        if r <= 0:
+            return
+        self.hole(x, y, r)
+
 
     def render(self):
+        #TODO: [ML] Validate design.  If validation fails, then output a problem to the user
+        # e.g. Is the height too small for the angle and depth?
+
         # adjust to the variables you want in the local scope
         x, y, h = self.x, self.y, self.h
         front = self.frontWallHeight
@@ -105,4 +134,5 @@ class DisplayShelf(Boxes): # change class name here and below
         self.generate_shelves()
 
         if self.include_back:
-            self.rectangularWall(x, h, "efef", label="back wall", move="up")
+            self.rectangularWall(x, h, "eFeF", label="back wall", move="up")
+
